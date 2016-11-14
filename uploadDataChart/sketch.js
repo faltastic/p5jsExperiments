f56d1fe031cebbca82849495ca0765b00e65c586
faltastic <youssef.faltas@gmail.com>
2016-11-14 21:24:22 +0100

    uploadDataChart

--- uploadDataChart/sketch.js
+++ uploadDataChart/sketch.js
@@ -8,37 +8,33 @@ var labels = [];
 var sel;
 var selVal = 0;
 var fileDiv;
-var n =0;
-var graphData=[];
-
+var n = 100;
+var lastn = 99;
+var graphData = [];
+var t = 0;
 var haveFile = false;
+var fileDivHTml = "";
 
-var fileDivHTml ="";
 function setup() {
-    
     //radio.position(x,y);
     var fileSelect = createFileInput(gotFile);
-    
-    var cnv = createCanvas(0.75*windowWidth, windowHeight/2);
+    var cnv = createCanvas(0.75 * windowWidth, windowHeight / 2);
     //createCanvas(100, 100);
-   cnv.parent('sketch-holder');
+    cnv.parent('sketch-holder');
     sel = createSelect();
     sel.position(50, 100);
-   // radio.position(0.1*width,windowHeight/2);
-    
-    fileDiv =select('#dataTable');
+    // radio.position(0.1*width,windowHeight/2);
+    fileDiv = select('#dataTable');
+    frameRate(4);
 }
 // file is a p5.File object that has metadata, and the file's contents
 function gotFile(file) {
     // Make a div to display info about the file
-    
     //console.log(radio);
-    fileDivHTml = "" ;
+    fileDivHTml = "";
     //radio = "defaultradio0";
-     sel = createSelect();
-     
+    sel = createSelect();
     sel.position(50, 100);
-    
     //file.name +  ' - ' + file.subtype + ' - ' + file.size + ' bytes';
     // Assign a CSS class for styling (see index.html)
     //fileDiv.class('file');
@@ -48,10 +44,10 @@ function gotFile(file) {
         img.class('thumb');
     }
     else if (file.type === 'text') {
-         entries = [];
+        entries = [];
         newdata = [];
         labels = [];
-        data=[];
+        data = [];
         successFunction(file.data);
     }
 }
@@ -86,7 +82,7 @@ function successFunction(data) {
                 }
                 else {
                     labels.push(rowCells[rowCell]);
-                   // console.log(rowCells[rowCell]);
+                    // console.log(rowCells[rowCell]);
                 }
                 table += '</td>';
             }
@@ -106,83 +102,83 @@ function successFunction(data) {
     table += '<br />';
     fileDiv.html(table);
     //$('dataTable').append(table);
-   // console.log(entries)  ;
+    // console.log(entries)  ;
     cleanupData();
     newRadio();
     haveFile = true;
+    t = 0;
 }
 
 function cleanupData() {
     data = [];
     var j = 0;
-    for(var i=0; i<entries.length; i++){
-      data[i] = [];
+    for (var i = 0; i < entries.length; i++) {
+        data[i] = [];
     }
-    
-    for(var i=0; i< newdata.length; i++){
-      j = i%entries.length;
-      data[j].push(newdata[i]);
+    for (var i = 0; i < newdata.length; i++) {
+        j = i % entries.length;
+        data[j].push(newdata[i]);
     }
-    
- 
 }
 
-function newRadio(){
-    
-    for(var i=0; i<entries.length; i++){
-      sel.option(entries[i],i);
+function newRadio() {
+    for (var i = 0; i < entries.length; i++) {
+        sel.option(entries[i], i);
     }
- // radio.style('width', '600px');
+    // radio.style('width', '600px');
     textAlign(CENTER);
-  //  radio.style('float', 'none'); 
-
+    //  radio.style('float', 'none'); 
 }
 
-//function processData(allText) {
-//    var allTextLines = allText.split(/\r\n|\n/);
-//    var headers = allTextLines[0].split(',');
-//    var lines = [];
-//    for (var i = 1; i < allTextLines.length; i++) {
-//        var data = allTextLines[i].split(',');
-//        if (data.length == headers.length) {
-//            var tarr = [];
-//            for (var j = 0; j < headers.length; j++) {
-//                tarr.push(headers[j] + ":" + data[j]);
-//            }
-//            lines.push(tarr);
-//        }
-//    }
-//   // console.log(headers);
-//}
+function draw() {
+    if (haveFile) {
+        t++;
+        n = sel.value();
+        if (lastn != n) {
+            graphIt();
+        }
+    }     
+}
 
-
-function draw(){
-    
-if(haveFile){
-  //if (sel) {
-    background(225);
-  //var val = radio.value();
-  
-    var offset = 40; //height/10;
-    line(offset,offset,offset,height-offset+10);
-    line(offset-10,height-offset,width-offset,height-offset);
-    
-    text('graph data is '+sel.value(), width/2, height/2);
-    n = sel.value();
+function graphIt() {
+    lastn = n;
+    background(245);
+    strokeWeight(0.5);
+    stroke(50);
+    //var val = radio.value();
+    var offset = 60; //height/10;
+    line(offset, offset-10, offset, height - offset + 10);
+    line(offset - 10, height - offset, width - offset, height - offset);
+    //text('graph data is ' + sel.value(), width / 2, height / 2);
     graphData = data[n];
-        
-      
-  //}
-}else{
-      background(225);
-  //var val = radio.value();
-  
-  var offset = 40; //height/10;
-  line(offset,offset,offset,height-offset+10);
-  line(offset-10,height-offset,width-offset,height-offset);
-     
+    var dMin = Math.min.apply(Math, graphData);
+    var dMax = Math.max.apply(Math, graphData);
+    console.log(dMin + " " + dMax);
+    
+    var startL = dMin ;
+    var endL = dMax ;
+    for (var i = 0; i < 5; i++) {
+        var h = map(i, 0, 5, offset, height - offset);
+        if (dMax % 1 == 0) {
+            text(map(i, 0, 5, endL, startL).toFixed(0), offset / 2, h);
+        }
+        else {
+            text(map(i, 0, 5, endL, startL).toFixed(1), offset / 2, h);
+        }
+        line(offset, h, width - offset, h);
+    }
+    var x=[];
+    var y=[];
+    for(var i=0; i<labels.length;i++){
+        x[i] = 2*offset +(i*(width-offset) /labels.length);
+        text(labels[i], x[i], height-offset/2);
+    }
+    for(var i=0; i<graphData.length;i++){
+        y[i] = map(graphData[i], dMin, dMax, height-offset, offset);   
+    }
+    strokeWeight(2.0);
+    stroke(0);
+    for(var i=0; i<graphData.length-1;i++){
+        line(x[i],y[i],x[i+1],y[i+1]);   
+    }
 }
-  
-  //for(var i=0; i<data[n].length )      
-}
-
