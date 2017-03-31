869a55452ed17668fb2176da52e9059bf4281bd0
faltastic <youssef.faltas@gmail.com>
2017-03-31 16:55:13 +0200

    extra base

--- huda/en/pieces.js
+++ huda/en/pieces.js
@@ -68,17 +68,18 @@ var nPhotos =[
     1
 ];
 
+var extraBase = "p5jsExperiments/huda/en/"
 
 for (var i = 0; i < N-1; i++) {
    preWork[i]={};
    preWork[i]["order"]=(i+1);
    preWork[i]["id"]="pc-"+(i+1);
    preWork[i]["categories"]= imgCat[i];
-   preWork[i]["thumb"] = "img/all/thumbs/"+ (i +1)+ ".png";
+   preWork[i]["thumb"] = extraBase + "img/all/thumbs/"+ (i +1)+ ".png";
    preWork[i]["nPhotos"]=nPhotos[i];
    preWork[i]["img"] =[];
     for (var j = 0; j < preWork[i]["nPhotos"]; j++) {
-        preWork[i]["img"][j] = "img/all/"+ (i+1) + "-"+(j+1)+".jpg";
+        preWork[i]["img"][j] = extraBase + "img/all/"+ (i+1) + "-"+(j+1)+".jpg";
     }
    preWork[i]["caption"] = imgCaption[i] ;
 
